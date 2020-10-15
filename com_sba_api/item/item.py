66f637dd4b2bc69b228511f0364b347a9ce8e666
salt <arcaorm00@gmail.com>
2020-10-15 13:26:43 +0900

    13:26

--- com_sba_api/item/item.py
+++ com_sba_api/item/item.py
@@ -1,6 +1,10 @@
-class Item:
-    
-    def __init__(self):
-        ...
+from com_sba_api.ext.db import Base
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
 
-    
+class Item(Base):
+
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    price = Column(String)
--- com_sba_api/user/user.py
+++ com_sba_api/user/user.py
@@ -1,4 +1,26 @@
-class User:
+from com_sba_api.ext.db import Base
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.dialects.mssql import DECIMAL, VARCHAR
 
-    def __init__(self):
-        ...
+class User(Base):
+
+    __tablename__ = "users"
+    __table_args__ = {"mysql_collate": 'utf8_general_ci'}
+
+    id = Column(Integer, primary_key=True, index=True)
+    userid = Column(VARCHAR(30))
+    password = Column(VARCHAR(30))
+    name = Column(VARCHAR(30))
+
+    def __repr__(self):
+        return f'User(id={self.id}, userid={self.userid}, password={self.password}, name={self.name})'
+
+engine = create_engine('mysql+mysqlconnector://root:munnin00@127.0.0.1/mariadb?charset=utf8', encoding='utf8', echo=True)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+session.add(User(userid='tom', password='1', name='thomas'))
+query = session.query
+
+session.commit()
